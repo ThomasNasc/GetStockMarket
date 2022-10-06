f61a99e11e2f7262baad067aa38887bb3750ce2a
ThomasNasc <thomas_n2008@hotmail.com>
2022-10-06 15:53:57 -0300

    deploying 1.1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 import List from "../components/List";
 import styles from "../styles/Home.module.css";
@@ -7,12 +5,6 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
       <Header />
       <List />
     </div>
